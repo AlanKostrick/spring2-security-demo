bbcea61438bf1a4a9d5374f258161c079c33d742
YourGitHubName <alan@wecancodeit.org>
2018-11-15 10:30:37 -0500

    add static recources to ant matchers permit all

--- src/main/java/org/wecancodeit/securitydemo/WebSecurityConfig.java
+++ src/main/java/org/wecancodeit/securitydemo/WebSecurityConfig.java
@@ -3,24 +3,32 @@ package org.wecancodeit.securitydemo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled=true) // Enables @PreAuthorize, etc.
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
 		.authorizeRequests()
 		.antMatchers("/admin/**").hasRole("ADMIN")
+		.antMatchers("/h2-console/**", "/css/**", "/js/**").permitAll()
 		.anyRequest().authenticated()
 		.and()
 	.formLogin()
@@ -46,10 +54,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
           .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
     }
     
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
   
     
 }
